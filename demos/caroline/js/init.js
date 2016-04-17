610ce20ec5c1e47f6fedcbf2a8a25cda6ce62717
Sasha <sasha16200@gmail.com>
2016-04-17 11:43:27 +0300

    v. 1.0.1
    add Caroline to demos

---
+++ demos/caroline/js/init.js
@@ -0,0 +1,24 @@
+$(function(){
+	
+	$('.slider1').mobilyslider({
+		bulletsHide: true
+	});
+	
+	$('.slider2').mobilyslider({
+		transition: 'vertical',
+		animationSpeed: 500,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		pauseOnHover: true,
+		bullets: false
+	});
+	
+	$('.slider3').mobilyslider({
+		transition: 'fade',
+		animationSpeed: 800,
+		bullets: true,
+		arrowsHide: false
+	});
+	
+	
+});
